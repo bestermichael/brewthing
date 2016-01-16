5677b4c6107926667baa6a703cc786bd9ad688e8
Michael Bester <bestermichael@gmail.com>
2016-01-16 14:02:03 +0200

    Added PID and multiprocessing to get the temperature control working

--- brewthing.py
+++ brewthing.py
@@ -1,6 +1,13 @@
 import os, glob, time, threading
 from flask import Flask, render_template, jsonify, request, g, redirect, url_for, flash
 from flask.ext.socketio import SocketIO, emit
+from pid import pidpy as PIDController
+from multiprocessing import Process, Pipe, Queue, current_process
+from Queue import Full
+from subprocess import Popen, PIPE, call
+from datetime import datetime
+import time, random, serial, os
+import RPi.GPIO as GPIO
 
 os.system('modprobe w1-gpio')  # Turns on the GPIO module
 os.system('modprobe w1-therm') # Turns on the Temperature module
@@ -12,13 +19,18 @@ app.config.from_object(__name__)
 app.config['SECRET_KEY'] = 'Secret!'
 socketio = SocketIO(app)
 
-
+global current_temp, parent_conn, parent_connB, parent_connC, statusQ, statusQ_B, statusQ_C
 
 
 @app.route('/', methods=['GET'])
 def main():
     start_temp_thread()
     return render_template('index.html')
+
+@app.route('/auto', methods=['GET', 'POST'])
+def automode():
+    
+    return render_template('auto.html')
 
 
 @socketio.on('connect_event', namespace='/events')
@@ -29,8 +41,6 @@ def test_message(message):
 '''----------------------------'''
 ''' THREADS and THREAD METHODS '''
 '''----------------------------'''
-
-
 
 def start_temp_thread():
     temp_thread = TempThread()
@@ -44,21 +54,265 @@ class TempThread(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
 
-
     def run(self):
         while True:
             print 'Sending Temperature: ' + str(self.get_temp_update())
             socketio.emit('current_temperature', {'data':'Temperature','current_temp': self.get_temp_update()}, namespace='/events')
             time.sleep(1)
             
-            
     def get_temp_update(self):
         # Read temperature from the Pi
         return read_temp()
 
+    
+''' ----------------------'''
+''' PID Processess '''
+''' ----------------------'''
 
 
+class param:
+    status = {
+        "mode" : "Auto",
+        "temp" : "65",
+        "tempUnits" : "C",
+        "elapsed" : "0",
+        "cycle_time" : 2.0,
+        "duty_cycle" : 0.0,
+        "boil_duty_cycle" : 60,
+        "set_point" : 68.0,
+        "boil_manage_temp" : 200,
+        "num_pnts_smooth" : 5,
+        "k_param" : 44,
+        "i_param" : 165,
+        "d_param" : 4             
+    }
+    
+    
+def unPackParam(paramStatus):
+    mode = paramStatus["mode"]
+    cycle_time = paramStatus["cycle_time"]
+    duty_cycle = paramStatus["duty_cycle"]
+    boil_duty_cycle = paramStatus["boil_duty_cycle"] 
+    set_point = paramStatus["set_point"] 
+    boil_manage_temp = paramStatus["boil_manage_temp"]
+    num_pnts_smooth = paramStatus["num_pnts_smooth"]
+    k_param = paramStatus["k_param"]
+    i_param = paramStatus["i_param"] 
+    d_param = paramStatus["d_param"] 
+    
+    return mode, cycle_time, duty_cycle, boil_duty_cycle, set_point, boil_manage_temp, num_pnts_smooth, \
+           k_param, i_param, d_param
+    
+         
+def gettempProc(conn, a):
+    p = current_process()
+    print 'Starting:', p.name, p.pid
+    
+    while(True):
+        t = time.time()
+        time.sleep(.5)
+        
+        #read the temperature
+        num = read_temp()
+        elapsed = "%.2f" % (time.time() - t)
+        conn.send([num, elapsed])
 
+        
+#Get time heating element is on and off during a set cycle time
+def getonofftime(cycle_time, duty_cycle):
+    duty = duty_cycle/100.0
+    on_time = cycle_time*(duty)
+    off_time = cycle_time*(1.0-duty)   
+    return [on_time, off_time]
+
+
+# Stand Alone Heat Process using GPIO
+def heatProcGPIO(cycle_time, duty_cycle, pinNum, conn):
+    p = current_process()
+    print 'Starting:', p.name, p.pid
+    if pinNum > 0:
+        GPIO.setup(pinNum, GPIO.OUT)
+        while (True):
+            while (conn.poll()): #get last
+                print "in while: conn.poll()"
+                cycle_time, duty_cycle = conn.recv()
+            conn.send([cycle_time, duty_cycle])  
+            if duty_cycle == 0:
+                GPIO.output(pinNum, OFF)
+                print "+ HEAT ON"
+                time.sleep(cycle_time)
+            elif duty_cycle == 100:
+                GPIO.output(pinNum, ON)
+                print "- HEAT OFF"
+                time.sleep(cycle_time)
+            else:
+                on_time, off_time = getonofftime(cycle_time, duty_cycle)
+                GPIO.output(pinNum, ON)
+                print "- HEAT OFF"
+                time.sleep(on_time)
+                
+                GPIO.output(pinNum, OFF)
+                print "+ HEAT ON"
+                time.sleep(off_time)
+                
+
+def heatProcGPIO2(cycle_time, duty_cycle, pinNum, conn) :
+    p = current_process()
+    print 'Starting:', p.name, p.pid
+    
+    while (True):
+        while (conn.poll()):
+            print "in while: conn.poll()"
+            cycle_time, duty_cycle = conn.recv()
+            conn.send([cycle_time, duty_cycle])  
+            if duty_cycle == 0:
+                #GPIO.output(pinNum, OFF)
+                print "+ HEAT ON"
+                time.sleep(cycle_time)
+            elif duty_cycle == 100:
+                #GPIO.output(pinNum, ON)
+                print "+ HEAT ON"
+                time.sleep(cycle_time)
+            else:
+                on_time, off_time = getonofftime(cycle_time, duty_cycle)
+                #GPIO.output(pinNum, ON)
+                print "+ HEAT ON"
+                time.sleep(on_time)
+                #GPIO.output(pinNum, OFF)
+                print "+ HEAT ON"
+                time.sleep(off_time)
+    
+
+def tempControlProc(statusQ, pinNum, conn):
+    mode, cycle_time, duty_cycle, boil_duty_cycle, set_point, boil_manage_temp, num_pnts_smooth, \
+        k_param, i_param, d_param = unPackParam(param.status);
+        
+    p = current_process()
+    print "Starting", p.name, p.pid
+    
+    # Connections and Pipes for getting the temperature
+    parent_conn_temp, child_conn_temp = Pipe()
+    ptemp = Process(name = "gettempProc", target=gettempProc, args=(child_conn_temp, 0))
+    ptemp.daemon = True
+    ptemp.start()
+    
+    # Connections and Pipes for the heading
+    parent_conn_heat, child_conn_heat = Pipe()      
+   
+    print "Pin Number is:", pinNum
+    print "cycle_time", cycle_time
+    print "duty_cycle", duty_cycle
+    
+    pheat = Process(name = "heatProcGPIO", target=heatProcGPIO, args=(cycle_time, duty_cycle, pinNum, child_conn_heat))
+    pheat.daemon = True
+    pheat.start()
+    
+    
+    temp_ma_list = []
+    manage_boil_trigger = False
+    readyPIDcalc = False
+    
+    while (True):
+            readytemp = False
+            while parent_conn_temp.poll():
+                temp_C, elapsed = parent_conn_temp.recv()
+                
+                if temp_C == -99:
+                    print "Bad Temp Reading - retry"
+                    continue
+                
+                temp_str = "%3.2f" % temp_C
+                
+                readytemp = True
+                
+            if readytemp:
+                print "in ready temp, mode is: ", mode
+                if mode == "Auto":
+                    print "in auto mode----****"
+                    temp_ma_list.append(temp_C)
+
+                    #smooth data
+                    temp_ma = 0.0 #moving avg init
+                    while (len(temp_ma_list) > num_pnts_smooth):
+                        temp_ma_list.pop(0) #remove oldest elements in list
+
+                    if (len(temp_ma_list) < num_pnts_smooth):
+                        for temp_pnt in temp_ma_list:
+                            temp_ma += temp_pnt
+                        temp_ma /= len(temp_ma_list)
+
+                    else: #len(temp_ma_list) == num_pnts_smooth
+                        for temp_idx in range(num_pnts_smooth):
+                            temp_ma += temp_ma_list[temp_idx]
+                        temp_ma /= num_pnts_smooth
+
+                    print "Check if readyPIDcalc is set to true: ", readyPIDcalc
+                    
+                    #calculate PID every cycle
+                    if (readyPIDcalc == True):
+                        print "Calculating the PID Cycle"
+                        print " -- temp_ma", temp_ma
+                        print " -- set_point", set_point
+                        pid = PIDController.pidpy(cycle_time, k_param, i_param, d_param) #init pid
+                        duty_cycle = pid.calcPID_reg4(temp_ma, set_point, True)
+                        #send to heat process every cycle
+                        parent_conn_heat.send([cycle_time, duty_cycle])
+                        readyPIDcalc = False
+                        print "set readyPIDCalc = False"
+
+                if mode == "boil":
+                    if (temp_C > boil_manage_temp) and (manage_boil_trigger == True): #do once
+                        manage_boil_trigger = False
+                        duty_cycle = boil_duty_cycle
+                        parent_conn_heat.send([cycle_time, duty_cycle])
+
+
+                print "Current Temp: %3.2f deg %s, Heat Output: %3.1f%%" % (temp_C, "C", duty_cycle)
+
+                readytemp == False
+
+                #if only reading temperature (no temp control)
+                if readOnly:
+                    continue
+                
+            while parent_conn_heat.poll(): #Poll Heat Process Pipe
+                cycle_time, duty_cycle = parent_conn_heat.recv() #non blocking receive from Heat Process
+                readyPIDcalc = True
+                print "set readyPIDCalc = True"
+            
+            readyPOST = False
+            while conn.poll(): #POST settings - Received POST from web browser or Android device
+                paramStatus = conn.recv()
+                mode, cycle_time, duty_cycle_temp, boil_duty_cycle, set_point, boil_manage_temp, num_pnts_smooth, \
+                    k_param, i_param, d_param = unPackParam(paramStatus)
+                readyPost = True
+
+            if readyPOST == True:
+                if mode == "Auto":
+                    print "Auto selected"
+                    pid = PIDController.pidpy(cycle_time, k_param, i_param, d_param) #init pid
+                    duty_cycle = pid.calcPID_reg4(temp_ma, set_point, True)
+                    parent_conn_heat.send([cycle_time, duty_cycle])
+
+                if mode == "Boil":
+                    print "Boil selected"
+                    boil_duty_cycle = duty_cycle_temp
+                    duty_cycle = 100 #full power to boil manage temperature
+                    manage_boil_trigger = True
+                    parent_conn_heat.send([cycle_time, duty_cycle])
+
+                if mode == "manual":
+                    print "manual selected"
+                    duty_cycle = duty_cycle_temp
+                    parent_conn_heat.send([cycle_time, duty_cycle])
+                if mode == "off":
+                    print "off selected"
+                    duty_cycle = 0
+                    parent_conn_heat.send([cycle_time, duty_cycle])
+                readyPOST = False
+            time.sleep(.01)
+            
+            
 
 
 '''------------------------'''
@@ -95,6 +349,21 @@ def read_temp():
     return temp_c   
 
 
+
+
     
 if __name__ == '__main__':
+    
+    GPIO.setmode(GPIO.BCM)
+    ON = 0
+    OFF = 1
+    readOnly = False
+    current_temp = 10
+    pinNum = 17
+    statusQ = Queue(2)
+    parent_conn, child_conn = Pipe() 
+    
+    p = Process(name = "TemperatureControlProcess", target=tempControlProc, args=(statusQ, pinNum, child_conn))
+    p.start()
+    
     socketio.run(app, host='0.0.0.0')
